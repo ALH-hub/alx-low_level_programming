ff89af42254e6334f33909e103e7868109b1e875
Alhadji Oumate <alhadjioum0@gmail.com>
2023-03-19 16:52:00 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: print a sentence using puts function
+ * Return: always 0
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
