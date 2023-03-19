ca87cf6b29d00e8f45437d2b85ac77633d263b1d
Alhadji Oumate <alhadjioum0@gmail.com>
2023-03-19 16:58:28 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print a sentence using printf function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
+}
