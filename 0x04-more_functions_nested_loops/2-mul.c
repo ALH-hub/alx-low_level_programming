01a57b8584ef19b3301216bd14122f0ea42f9d1e
Alhadji Oumate <alhadjioum0@gmail.com>
2023-03-23 14:50:00 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * Description: multiply two numbers
+ * @a: one of the numbers to multiply
+ * @b: another one of the numbers
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
