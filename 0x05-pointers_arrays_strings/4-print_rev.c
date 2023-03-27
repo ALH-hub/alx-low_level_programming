dcc58ff40b2a39ff0d3d24348652c00903aaa548
Alhadji Oumate <alhadjioum0@gmail.com>
2023-03-27 14:37:40 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse order
+ * @s: the string to reverse
+ * return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
