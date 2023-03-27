22d838af60dcb0645ad3a24d24a8e4dfc7036097
Alhadji Oumate <alhadjioum0@gmail.com>
2023-03-27 15:36:30 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - print other characters of a string
+ * @str: string to be processed
+ * return: void
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int i = 1, j;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (j = 0; i < len; j++)
+	{
+		if (i % 2 != 0)
+		{
+			_putchar(str[j]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
