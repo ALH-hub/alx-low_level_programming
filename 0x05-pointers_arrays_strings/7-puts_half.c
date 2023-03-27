f6769953e71f7ca67861d708212ce74e844b5047
Alhadji Oumate <alhadjioum0@gmail.com>
2023-03-27 16:05:53 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,20 +11,20 @@ void puts_half(char *str)
 	int len = 0, i;
 
 	while (str[len] != '\0')
-		{
-			len++;
-		}
+	{
+		len++;
+	}
 
 	if (len % 2 == 0)
 	{
-		for (i = len/2; i <= len; i++)
+		for (i = len / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = (len-1)/2; i <= len; i++)
+		for (i = (len - 1) / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
