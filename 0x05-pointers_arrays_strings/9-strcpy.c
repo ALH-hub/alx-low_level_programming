42beb681f502994cbc399abfae69a9c0edca1d9c
Alhadji Oumate <alhadjioum0@gmail.com>
2023-03-27 16:59:43 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string to another
+ * @src: the source string
+ * @dest: the buffer to contain the copied string
+ * Return: a pointer of type char to the buffer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = 0;
+
+	while (src[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
