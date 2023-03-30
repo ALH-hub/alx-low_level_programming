54e95a2601d5ceb1e0c7104f5aaab93b3a455bd9
Alhadji Oumate <alhadjioum0@gmail.com>
2023-03-30 09:50:28 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string to n number of bytes
+ * @dest: recepient of the output
+ * @src: the string to copy dest
+ * @n: number of bytes to copy
+ * Return: character pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
