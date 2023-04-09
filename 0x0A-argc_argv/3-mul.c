3d98a3f2781746974e9543dae3d3a1e381f0e1b3
Alhadji Oumate <alhadjioum0@gmail.com>
2023-04-08 18:47:27 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,18 @@ int main(int argc, char *argv[])
 {
 	int n1, n2, prod = 1;
 
-	if (argc <= 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		n1 = atoi(argv[1]);
 		n2 = atoi(argv[2]);
 		prod = n1 * n2;
 		printf("%d\n", prod);
+		
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
