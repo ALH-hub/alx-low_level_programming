10eacef8e97e2bca434fb1d42328122dc54ad147
Alhadji Oumate <alhadjioum0@gmail.com>
2023-04-18 16:15:43 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - structure type
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ * Description: more description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
