182c4cf956eef8e07c2823ca1d352a44e5809e88
Alhadji Oumate <alhadjioum0@gmail.com>
2023-04-18 17:19:19 -0700

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <limits.h>
+#include "dog.h"
+
+/**
+ * print_dog - print a strcture
+ * @d: the structure variable;
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
