92633c5ea73ec46de43dcffa96e12bb96d303eea
Alhadji Oumate <alhadjioum0@gmail.com>
2023-06-12 19:49:15 +0100

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
-#include <limits.h>
 #include <string.h>
 #include "main.h"
 
 /**
  * str_concat - concatenate two strings
- * @s1: the first string
+ * @s1: tcohe first string
  * @s2: the second string to be concatenated
  * Return: concatenated string on success and NULL on failure
  */
@@ -23,47 +22,36 @@ char *str_concat(char *s1, char *s2)
 			return (NULL);
 		for (i = 0; i < len; i++)
 			str[i] = s1[i];
-		str[len] = '\0';
 		return (str);
 	}
-	else
+	if (s1 == NULL && s2 != NULL)
 	{
-		if (s1 == NULL && s2 != NULL)
-		{
-			len = strlen(s2);
-			str = malloc(sizeof(char) * (len + 1));
-			if (str == NULL)
-				return (NULL);
-			for (j = 0; j < len; j++)
-				str[j] = s2[j];
-			str[len] = '\0';
-			return (str);
-		}
-		else
-		{
-			if (s1 == NULL && s2 == NULL)
-			{
-				str = malloc(sizeof(char));
-				if (str == NULL)
-					return (NULL);
-				str[0] = '\0';
-				return (str);
-			}
-			else
-			{
-				len = strlen(s1) + strlen(s2);
-				str = malloc(sizeof(char) * (len + 1));
-				if (str == NULL)
-					return (NULL);
-				for (i = 0; i < strlen(s1); i++)
-					str[i] = s1[i];
-				for (j = i; j < len; j++)
-				{
-					str[j] = s2[k];
-					k++;
-				}
-				return (str);
-			}
-		}
+		len = strlen(s2);
+		str = malloc(sizeof(char) * (len + 1));
+		if (str == NULL)
+			return (NULL);
+		for (j = 0; j < len; j++)
+			str[j] = s2[j];
+		return (str);
 	}
+	if (s1 == NULL && s2 == NULL)
+	{
+		str = malloc(sizeof(char));
+		if (str == NULL)
+			return (NULL);
+		str[0] = '\0';
+		return (str);
+	}
+	len = strlen(s1) + strlen(s2);
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < strlen(s1); i++)
+		str[i] = s1[i];
+	for (j = i; j < len; j++)
+	{
+		str[j] = s2[k];
+		k++;
+	}
+	return (str);
 }
