26e8e1949be4794e6dac8f56f5f7db4d71a35c39
Alhadji Oumate <alhadjioum0@gmail.com>
2023-06-19 17:55:39 +0100

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,41 @@
 #include <stdlib.h>
-#include <limits.h>
 #include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *str);
+
+/**
+ * _strcpy - copies a string to another
+ * @src: the source string
+ * @dest: the buffer to contain the copied string
+ * Return: a pointer of type char to the buffer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - return the length of the string
+ * @str: the string concerned
+ * Return: the length of the string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+		len++;
+	return (len);
+}
 
 /**
  * new_dog - create a new dog
@@ -14,15 +49,25 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
-	char *nc = "", *oc = "";
 
-	_strcpy(nc, name);
-	_strcpy(oc, owner);
+	if (name == NULL || age < 0 || owner == NULL)
+		return NULL;
+
 	n_dog = malloc(sizeof(dog_t));
 	if (n_dog == NULL)
 		return (NULL);
-	n_dog->name = name;
+
+	n_dog->name = malloc(sizeof(char) * _strlen(name) + 1);
+	if (n_dog->name == NULL)
+		return (NULL);
+
+	n_dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
+	if (n_dog->owner == NULL)
+		return (NULL);
+
+	n_dog->name = _strcpy(n_dog->name, name);
+	n_dog->owner = _strcpy(n_dog->owner, owner);
 	n_dog->age = age;
-	n_dog->owner = owner;
+
 	return (n_dog);
 }
