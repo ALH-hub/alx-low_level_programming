70018b2181b4e6c42dfd8059a5b147a9308f9149
Alhadji Oumate <alhadjioum0@gmail.com>
2023-06-30 22:40:40 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,7 @@
 #include "lists.h"
-#include <unistd.h>
+#include <stdio.h>
 
 int _strlen(char *str);
-int _putchar(char c);
 
 /**
  * print_list - print the elements of a linked list
@@ -12,7 +11,7 @@ int _putchar(char c);
 
 size_t print_list(const list_t *h)
 {
-	int len = 0, i = 0;
+	int len = 0;
 	size_t n = 0;
 
 	if (h == NULL)
@@ -23,28 +22,11 @@ size_t print_list(const list_t *h)
 		if (h->str != NULL)
 		{
 			len = _strlen(h->str);
-			_putchar('[');
-			_putchar(len + '0');
-			_putchar(']');
-			_putchar(' ');
-
-			for (i = 0; i < len; i++)
-				_putchar(h->str[i]);
-
-			_putchar('\n');
+			printf("[%d] %s\n", len, h->str);
 		}
 		else
 		{
-			_putchar('[');
-			_putchar(48);
-			_putchar(']');
-			_putchar(' ');
-			_putchar('(');
-			_putchar('n');
-			_putchar('i');
-			_putchar('l');
-			_putchar(')');
-			_putchar('\n');
+			printf("[0] (nil)\n");
 		}
 		n++;
 		h = h->next;
@@ -66,16 +48,4 @@ int _strlen(char *str)
 	while (str[len] != '\0')
 		len++;
 	return (len);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
