0aedf0d01555232239c0938ad0d80d3966d0c776
Alhadji Oumate <alhadjioum0@gmail.com>
2023-07-04 23:04:04 +0100

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * list_len - the length of a linked list
+ * listint_len - the length of a linked list
  * @h: the linked list
  * Return: the number of elements in a linked list
  */
 
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
-	int len = 0;
+	size_t len = 0;
 
 	if (h == NULL)
 		return (len);
