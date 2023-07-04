33e4422b4e40904bfb31a02cc3395589baae21c2
Alhadji Oumate <alhadjioum0@gmail.com>
2023-07-04 23:30:07 +0100

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
 /**
- * free_list - free a linked list
+ * free_listint - free a linked list
  * @head: the head of the list
  * Return: nothing
  */
 
-void free_list(list_t *head)
+void free_listint(listint_t *head)
 {
-	list_t *temp;
+	listint_t *temp;
 
 	while (head)
 	{
 		temp = head->next;
-		free(head->str);
 		free(head);
 		head = temp;
 	}
