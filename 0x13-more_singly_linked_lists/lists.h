dcd3cb330d404b249a223ebaf80f1efa03f547f4
Alhadji Oumate <alhadjioum0@gmail.com>
2023-07-04 22:49:17 +0100

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,14 +14,14 @@
 typedef struct listint_s
 {
     int n;
-    struct list_s *next;
-} list_t;
+    struct listint_s *next;
+} listint_t;
 
-size_t print_listint(const list_t *h);
-size_t listint_len(const list_t *h);
-list_t *add_nodeint(list_t **head, const char *str);
-list_t *add_nodeint_end(list_t **head, const char *str);
-void free_listint(list_t *head);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const char *str);
+listint_t *add_nodeint_end(listint_t **head, const char *str);
+void free_listint(listint_t *head);
 int _putchar(char c);
 
 #endif
