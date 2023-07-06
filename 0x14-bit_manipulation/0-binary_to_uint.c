fc0bc40d1c7976a8bb893b8072d68225f7da8af2
Alhadji Oumate <alhadjioum0@gmail.com>
2023-07-06 23:33:40 +0100

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert from binary to unsigned integer
+ * @b: the binary number to convert
+ * Return: converted number on success or NULL on failure
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, pow = 1;
+	int len;
+
+	if (b == '\0')
+		return (0);
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * pow;
+		pow *= 2;
+	}
+
+	return (num);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+alx course
+c language bit manipulation
