f6502d19cdb429700ed0aa19eb7a6630c4c5086f
Alhadji Oumate <alhadjioum0@gmail.com>
2023-07-06 23:54:49 +0100

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_binary - print a number in binary
+ * @n: the number to print in binary
+ * Return: nothin
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
