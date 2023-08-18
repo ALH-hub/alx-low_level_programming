55adb256d6513a21061833b0fd0074c3f9983d4d
Alhadji Oumate <alhadjioum0@gmail.com>
2023-08-18 02:01:46 +0100

    task 8: delete node in dlist

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete a node at an index in a dlist
+ * @head: head of the list
+ * @index: the index of the node
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head,
+		unsigned int index)
+{
+	dlistint_t *list = (*head);
+
+	if ((*head) == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (list == NULL)
+			return (-1);
+		list = list->next;
+	}
+
+	if (list == (*head))
+	{
+		(*head) = list->next;
+		if ((*head) != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		list->prev->next = list->next;
+		if (list->next != NULL)
+			list->next->prev = list->prev;
+	}
+	
+	free(list);
+	return(1);
+}
