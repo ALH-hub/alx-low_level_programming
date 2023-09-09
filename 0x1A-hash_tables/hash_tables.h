0db3ce393601ff2175b24234bf7818b1ecd9f81f
Alhadji Oumate <alhadjioum0@gmail.com>
2023-09-09 11:39:03 +0100

    header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -31,3 +34,5 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
