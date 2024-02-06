c74ba45a9c389d8d0fafde20bfd750a8ebdfe373
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-06 12:28:40 +0100

    linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,15 +12,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array)
+	if (array == NULL)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			printf("Value checked array[%d] = [%d]\n", (int) i, array[(int) i]);
-			if (value == array[i])
-				return ((int) i);
-		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return ((int) i);
 	}
-
+	
 	return (-1);
 }
