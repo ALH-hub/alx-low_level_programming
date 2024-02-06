bef261689cbcc795d9fb3b0ffecc84cdf4b18424
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-06 12:42:14 +0100

    post method with parameters"

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,13 +14,13 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (value == array[i])
-			return ((int) i);
+		if (array[i] == value)
+			return (i);
 	}
-	
+
 	return (-1);
 }
